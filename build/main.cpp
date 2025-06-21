3887032b98c04f2a75b277d02cdeeffed5bb7e2b
Hussein Salehi <99806741+husa2101@users.noreply.github.com>
2025-06-21 03:47:56 +0200

    Add files via upload

---
+++ build/main.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+
+struct Adress
+{
+    std::string street;
+    int zip;
+    std::string city;
+};
+
+struct Person
+{
+    std::string name;
+    std::string id;
+    Adress location;
+};
+
+void print(const Person &person){
+    std::cout << person.name << "\n" 
+    << person.id << "\n"
+    << person.location.street << " " << person.location.zip<< " "<< person.location.city << "\n\n"; 
+
+}
+
+void read(std::ifstream & file){
+    std::vector<Person> vPerson;
+
+    std::string line;
+    while(std::getline(file, line)){
+        Person person;
+        person.name = line;
+
+        std::getline(file, line);
+        person.id = line;
+
+        std::getline(file, line);
+
+        std::stringstream str(line);
+        str >> person.location.street >> person.location.zip >> person.location.city;
+
+        vPerson.push_back(person);
+        
+
+    }
+
+    std::for_each(vPerson.begin(), vPerson.end(), print);
+ 
+}
+
+
+
+
+int main(){
+
+    std::ifstream file;
+    file.open("names.txt");
+
+    if(!file.is_open()){
+    std::cerr << "unable to open the file" << std::endl;
+    return 1;
+    }
+
+    read(file);
+
+    
+    file.close();
+
+    return 0;
+}
